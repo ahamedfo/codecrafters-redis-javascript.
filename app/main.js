41451973844291386c6fa8a4385c06882f81b43f
Ahamed Fofana <ahamedfofana12@gmail.com>
2024-11-29 01:24:24 -0100

    event listener added

--- app/main.js
+++ app/main.js
@@ -4,15 +4,13 @@ const net = require("net");
 console.log("Logs from your program will appear here!");
 
 const server = net.createServer((connection) => {
-    connection.write('+PONG\r\n');
+    // Handle connection
+
+    connection.on('data', (data) => {
+        connection.write('+PONG\r\n');
+    });
 
 });
 
 server.listen(6379, "127.0.0.1")
 
-// Uncomment this block to pass the first stage
-// const server = net.createServer((connection) => {
-//   // Handle connection
-// });
-//
-// server.listen(6379, "127.0.0.1");
